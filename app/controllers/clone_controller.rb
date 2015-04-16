547e4e5c1e8a73270259c3156d90bc24e2a1c5ce
Mauricio Camayo <mauricio.camayo@gmail.com>
2015-04-16 16:47:51 -0500

    Changed the visualization for issues, added the stock filters

--- app/controllers/clone_controller.rb
+++ app/controllers/clone_controller.rb
@@ -1,42 +1,84 @@
 class CloneController < ApplicationController
   unloadable
 
-  def index
-    @project = Project.find(params[:project_id])
-    if Setting.plugin_redmine_clone_by_version.has_key?("target_version")
-      @issues = Issue.where("fixed_version_id = " + Setting.plugin_redmine_clone_by_version['target_version'].at(0))
-    else
-      @issues = {}
-    end
-    if Setting.plugin_redmine_clone_by_version.has_key?("group")
-      @users = User.find_all_by_type('User')
-    else
-      @users = User.where("type = 'User'").all
-    end
-  end
   
-  def doclone
-    @created_issues = Array.new
-    if Setting.plugin_redmine_clone_by_version.has_key?("new_status")
-      status = IssueStatus.find(Setting.plugin_redmine_clone_by_version['new_status'].at(0))
+  helper :issues
+  helper :projects
+  include ProjectsHelper
+  helper :custom_fields
+  include CustomFieldsHelper
+  helper :issue_relations
+  include IssueRelationsHelper
+  helper :queries
+  include QueriesHelper
+  helper :sort
+  include SortHelper
+  include IssuesHelper
+  
+  def index
+#    javascript_include_tag('context_menu')
+      
+    @project = Project.find(params[:project_id])
+    retrieve_query
+    @issues = @query.issues
+    if Setting.plugin_redmine_clone_by_version.has_key?("group")
+      @users = User.status('1').in_group(Setting.plugin_redmine_clone_by_version["group"].at(0))
     else
-      status = IssueStatus.find(1)
+      @users = User.where("type = 'User' and status = '1'").all
     end
-    for u in params["user_ids"] 
-      user = User.find(u.to_i)
-      for i in params["issue_ids"]
-        issue = Issue.find(i.to_i)
-        clone = issue.dup
-        clone.assigned_to = user
-        clone.status = status
-        clone.done_ratio = 0
-        clone.closed_on = ""
-        clone.author = User.current
-        clone.fixed_version = Version.find(params["target_version"].at(0).to_i)
-        clone.save
-        @created_issues << clone
+    
+    if params[:commit] == 'doclone'
+      if !params['user_ids'] || params['user_ids'].count < 1 || !params['issue_ids'] || params['issue_ids'].count < 1
+        flash.now[:error] = l(:selection_error)
+      else
+        @created_issues = Array.new
+        if Setting.plugin_redmine_clone_by_version.has_key?("new_status")
+          status = IssueStatus.find(Setting.plugin_redmine_clone_by_version['new_status'].at(0))
+        else
+          status = IssueStatus.find(1)
+        end
+        for u in params["user_ids"] 
+          user = User.find(u.to_i)
+          for i in params["issue_ids"]
+            issue = Issue.find(i.to_i)
+            clone = issue.dup
+            clone.assigned_to = user
+            clone.status = status
+            clone.done_ratio = 0
+            clone.closed_on = ""
+            clone.author = User.current
+            clone.fixed_version = Version.find(params["target_version"].at(0).to_i)
+            clone.save
+            @created_issues << clone
+          end
+        end
+        flash[:notice] = "#{@created_issues.count}" + l(:issues_cloned)
+        @issues = @created_issues
       end
     end
   end
 
+  private
+  def retrieve_query
+
+    if params[:set_filter] || session[:clone_query].nil? || session[:clone_query][:project_id] != (@project ? @project.id : nil)
+      # Give it a name, required to be valid
+      @query = CloneQuery.new(:name => "_")
+      @query.project = @project
+      @query.build_from_params(params)
+      session[:clone_query] = {:project_id => @query.project_id,
+        :filters => @query.filters,
+        :group_by => @query.group_by,
+        :column_names => @query.column_names}
+    else
+      # retrieve from session
+      @query = CloneQuery.new(:name => "_",
+        :filters => session[:clone_query][:filters] || session[:agile_query][:filters],
+        :group_by => session[:clone_query][:group_by],
+        :column_names => session[:clone_query][:column_names]
+      )
+      @query.project = @project
+    end
+    @chart = params[:chart] || "issues_burndown"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
-get 'clone', :to => 'clone#index'
-post 'clone/doclone', :to => 'clone#doclone'
+#get 'clone', :to => 'clone#index'
+#post 'clone/doclone', :to => 'clone#doclone'
+
+
+resources :clone_queries
+
+
+get '/projects/:project_id/clone/', :to => "clone#index"
+post '/projects/:project_id/clone/doclone', :to => 'clone#doclone'
